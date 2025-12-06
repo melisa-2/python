c5afdcad5e4c810d6fcda21496dad90da5e779cb
Melisa <melisaterstenaa@gmail.com>
2025-12-06 14:35:02 +0100

    lesson3

--- Module4/for_loops.py
+++ Module4/for_loops.py
@@ -19,3 +19,50 @@ for num in numbers:
     if num > max:
         max = num
 print("Maksimumi eshte:" , max)
+
+count=1
+
+while count<=5:
+    print("Rrite vleren per nje: ", count)
+    count += 1
+
+numbers= [1,2,3,4,5,6]
+target=4
+
+for number in numbers:
+    print(number)
+    if number == target:
+        print("Target found")
+        break
+
+scores=[68,42,57,86,73,50,92,30]
+total=0
+count=0
+
+for score in scores:
+    if score<50:
+        continue
+    total+=score
+    count+=1
+
+mesatarja = total/count
+print("Mesatarja ka qene: ", mesatarja)
+
+while True:
+    user_input=input("Shtyp nje numer pozitiv: ")
+    if user_input.isnumeric():
+        number = int(user_input)
+        if number > 0:
+            break
+    print("Invalid. Try again")
+print("You enter a positive number")
+
+
+while True:
+    user_input=input("Shtyp nje numer cift: ")
+    if user_input.isnumeric():
+        number = int(user_input)
+        if number % 2 ==0:
+            break
+    print("Invalid. Try again")
+print("numer cift")
