9cc19f2298f439f9f9d7b094e5929daa4ec5c8b1
Melisa <melisaterstenaa@gmail.com>
2025-12-20 14:32:37 +0100

    lesson3

---
+++ Module6/using_pip.py
@@ -0,0 +1,3 @@
+import emoji
+
+print(emoji.emojize("python is fun:snake:"))
---
+++ Module7/user_input.py
@@ -0,0 +1,12 @@
+name = input("Enter your name:")
+print(f"Hello,{name}")
+
+age = int(input("Enter your age:"))
+print(type(age))
+
+number1 = int(input("Enter your first number:"))
+number2 = int(input("Enter your second number:"))
+
+result=number1+number2
+
+print(f"The sum of {number1} and {number2} is {result}")
